9fdb190d943eee4175bfda1521efecdfeb794b6d
Dan Cristian Bota <dan.cristian.bota@gmail.com>
2018-10-18 12:22:20 +0000

    add json_parser tests

--- spec/json_parser_spec.rb
+++ spec/json_parser_spec.rb
@@ -1,12 +1,20 @@
 require 'json_parser.rb'
+require 'csv'
 
-describe JsonFetcher do
-    describe '#get_json_from_url' do
+describe JsonParser do
+    describe '#get_csv_from_json' do
         url = 'https://academy-ruby.nyc3.digitaloceanspaces.com/movies.json'
-        subject {JsonFetcher.get_json_from_url(url)}
+        json = JsonFetcher.get_json_from_url(url)
+        subject {JsonParser.get_csv_from_json(json)}
         
+        it "is a String" do
+            expect(subject.class).to eq(String)
+        end
         it "is not empty" do
             expect(subject).not_to be_empty
+        end
+        it "is a valid CSV" do
+            # expect(subject).to eq(String)
         end
     end
 end
